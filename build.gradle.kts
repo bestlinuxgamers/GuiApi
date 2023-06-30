d3f6e96e17a02f9f74757d5938b904f7a650913c
bestlinuxgamers <52172848+bestlinuxgamers@users.noreply.github.com>
2023-06-30 02:56:28 +0200

    fix(gradle): fix wrong targetCompatibility for JavaCompile

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,15 +36,15 @@ dependencies {
 
 //task settings
 
-val targetCompatibility = JavaVersion.VERSION_1_8.toString()
+val targetProjectCompatibility = JavaVersion.VERSION_1_8.toString()
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = targetCompatibility
+    kotlinOptions.jvmTarget = targetProjectCompatibility
 }
 
 tasks.withType<JavaCompile> {
     sourceCompatibility = JavaVersion.current().toString()
-    targetCompatibility = targetCompatibility
+    targetCompatibility = targetProjectCompatibility
 }
 
 tasks.test {
